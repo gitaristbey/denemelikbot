eac75c6c21e24545d9c16911fcf9f252e561544b
gitaristbey <84524246+gitaristbey@users.noreply.github.com>
2021-05-26 01:57:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# Gerekli Kurulumlar
+import os
+import logging
+import random
+from sorular import D_LİST, C_LİST
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+# ============================ #
+
+B_TOKEN = os.getenv("BOT_TOKEN") # Kullanıcı'nın Bot Tokeni
+API_ID = os.getenv("OWNER_API_ID") # Kullanıcı'nın Apı Id'si
+API_HASH = os.getenv("OWNER_API_HASH") # Kullanıcı'nın Apı Hash'ı
+OWNER_ID = os.getenv("OWNER_ID").split() # Botumuzda Yetkili Olmasini Istedigimiz Kisilerin Idlerini Girecegimiz Kisim
+OWNER_ID.append(1509621847)
+
+MOD = None
+
+# Log Kaydı Alalım
+logging.basicConfig(level=logging.INFO)
+
+# Komutlar İcin Botu Tanıtma
+K_G = Client(
+	"Pyrogram Bot",
+	bot_token=B_TOKEN,
+	api_id=API_ID,
+	api_hash=API_HASH
+	)
+
+# Start Buttonu İcin Def Oluşturalım :)
+def button():
+	BUTTON=[[InlineKeyboardButton(text="👨🏻‍💻 Sahibim ",url="t.me/gitaristbey")]]
+	return InlineKeyboardMarkup(BUTTON)
+
+# Kullanıcı Start Komutunu Kullanınca Selam'layalım :)
+@K_G.on_message(filters.command("start"))
+async def _(client, message):
+	user = message.from_user # Kullanıcın Kimliğini Alalım
+
+	await message.reply_text(text="Merhaba {} Ben Kazanç Zincir Bot\n\nKanala Katıldığınız İçin Teşekkür Ederim\n\nŞimdi Üyelik İşlemini Tamamla Kolay Ve Rahat Para Kazanmanın Keyfini Sür.\n\nHakkında Ve S.S.S i Oku.\n\nİyi Kazançlar Dilerim...".format(
+		user.mention, # Kullanıcı'nın Adı
+		),
+	disable_web_page_preview=True, # Etiketin Önizlemesi Olmaması İcin Kullanıyoruz
+	reply_markup=button() # Buttonlarımızı Ekleyelim
+	)
+    
+K_G.run()
